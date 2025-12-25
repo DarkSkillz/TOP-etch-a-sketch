7c0c6435906bdad2c0ca71fb664a266ce9a2dc82
DarkSkillz <githubskillz@gmail.com>
2025-12-25 22:12:52 +0100

    Add a reset sketch button

--- app.js
+++ app.js
@@ -2,37 +2,45 @@
 const c = document.getElementById('canvas')
 const ctx = c.getContext("2d")
 
-let startX = 500
-let startY = 300
+let posX = 500
+let posY = 300
 let drawDistance = 10
 ctx.lineWidth = 2
-ctx.moveTo(startX,startY)
+ctx.moveTo(posX,posY)
 
 document.addEventListener("keydown", draw)
 
 function draw(e) {
     if (e.key == "ArrowRight" || e.key == "d") {
-        ctx.lineTo(startX + drawDistance, startY)
+        ctx.lineTo(posX + drawDistance, posY)
         ctx.stroke()
-        startX += drawDistance
-        ctx.moveTo(startX,startY)
+        posX += drawDistance
+        ctx.moveTo(posX,posY)
     }
     if (e.key == "ArrowLeft" || e.key == "q" || e.key == "a") {
-        ctx.lineTo(startX - drawDistance, startY)
+        ctx.lineTo(posX - drawDistance, posY)
         ctx.stroke()
-        startX -= drawDistance
-        ctx.moveTo(startX,startY)
+        posX -= drawDistance
+        ctx.moveTo(posX,posY)
     }
     if (e.key == "ArrowUp" || e.key == "z" || e.key == "w") {
-        ctx.lineTo(startX, startY - drawDistance)
+        ctx.lineTo(posX, posY - drawDistance)
         ctx.stroke()
-        startY -= drawDistance
-        ctx.moveTo(startX,startY)
+        posY -= drawDistance
+        ctx.moveTo(posX,posY)
     }
     if (e.key == "ArrowDown" || e.key == "s") {
-        ctx.lineTo(startX, startY + drawDistance)
+        ctx.lineTo(posX, posY + drawDistance)
         ctx.stroke()
-        startY += drawDistance
-        ctx.moveTo(startX,startY)
+        posY += drawDistance
+        ctx.moveTo(posX,posY)
     }
+}
+
+document.getElementById("reset").addEventListener("click", reset)
+
+function reset () {
+    ctx.clearRect(0,0,c.width,c.height)
+    ctx.beginPath()
+    ctx.moveTo(posX,posY)
 }
