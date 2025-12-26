f89303e76cd1e295722f4edc0d0d7a4566c6ccf7
DarkSkillz <githubskillz@gmail.com>
2025-12-26 01:15:16 +0100

    Add stylus functionality to code

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 /** @type {HTMLCanvasElement} */
 const c = document.getElementById('canvas')
 const ctx = c.getContext("2d")
+const c2 = document.getElementById("inner-canvas")
+const ctx2 = c2.getContext("2d")
 
 let posX = 500
 let posY = 300
+let stylusPosX = 500
+let stylusPosY = 300
 let drawDistance = 10
 ctx.lineWidth = 2
+ctx2.lineWidth = 2
 ctx.moveTo(posX,posY)
+ctx2.moveTo(stylusPosX,stylusPosY)
+ctx2.strokeStyle = "red"
 
 document.addEventListener("keydown", draw)
 
@@ -41,8 +48,53 @@ function draw(e) {
 
 document.getElementById("reset").addEventListener("click", reset)
 
-function reset () {
+function reset() {
     ctx.clearRect(0,0,c.width,c.height)
     ctx.beginPath()
     ctx.moveTo(posX,posY)
+}
+
+/* Stylus */
+
+document.addEventListener("keydown", drawStylus)
+
+function drawStylus(e) {
+    if (e.key == "ArrowRight" || e.key == "d") {
+        resetStylus()
+        ctx2.lineTo(stylusPosX + drawDistance, stylusPosY)
+        ctx2.stroke()
+        stylusPosX += drawDistance
+        ctx2.moveTo(stylusPosX,stylusPosY)
+    }
+    /* support for both AZERTY and QWERTY */
+    if (e.key == "ArrowLeft" || e.key == "q" || e.key == "a") {
+        resetStylus()
+        ctx2.lineTo(stylusPosX - drawDistance, stylusPosY)
+        ctx2.stroke()
+        stylusPosX -= drawDistance
+        ctx2.moveTo(stylusPosX,stylusPosY)
+    }
+    /* support for both AZERTY and QWERTY */
+    if (e.key == "ArrowUp" || e.key == "z" || e.key == "w") {
+        resetStylus()
+        ctx2.lineTo(stylusPosX, stylusPosY - drawDistance)
+        ctx2.stroke()
+        stylusPosY -= drawDistance
+        ctx2.moveTo(stylusPosX,stylusPosY)
+    }
+    if (e.key == "ArrowDown" || e.key == "s") {
+        resetStylus()
+        ctx2.lineTo(stylusPosX, stylusPosY + drawDistance)
+        ctx2.stroke()
+        stylusPosY += drawDistance
+        ctx2.moveTo(stylusPosX,stylusPosY)
+    }
+}
+
+document.getElementById("reset").addEventListener("click", resetStylus)
+
+function resetStylus() {
+    ctx2.clearRect(0,0,c2.width,c2.height)
+    ctx2.beginPath()
+    ctx2.moveTo(stylusPosX,stylusPosY)
 }
